8a8a127dacd84e3da56e1da1b1e928dbc3e0e598
deborahhuang <deborah.huang@nec.com.tw>
2020-12-14 14:20:01 +0800

    美化按鈕元件

--- src/views/List.vue
+++ src/views/List.vue
@@ -2,18 +2,17 @@
 <template>
   <div class="todoList">
     <header>
-      <section>
-        <h1>My To-Do List</h1>
-        <input type="text" v-model="todo" @keyup.enter="addTodo" placeholder="加入ToDo" />
-      </section>
+      <h1>My To-Do List</h1>
+      <input type="text" v-model="todo" @keyup.enter="addTodo" placeholder="加入ToDo" />
     </header>
     <section class="items">
       <h2 class="pending">待辦中<span class="badge">{{todoList.length}}</span></h2>
       <h2 class="done">已完成<span class="badge">{{doneList.length}}</span></h2>
       <label v-for="(item, index) in allList" :key="'info-'+ index" :class="{ isDone: item.done }">
+        <div class="check-style"></div>
         <input type="checkbox" @change="changeTodo(item)">
-        <span>{{item.todo}}</span>
-        <a @click="deleteTodo(index,true)">-</a>
+        {{item.todo}}
+        <div class="icon-cross" @click="deleteTodo(index,true)"></div>
       </label>
     </section>
   </div>
